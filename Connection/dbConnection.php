77538d43a78dc374639e19806837b870b5243c45
nickdavis3188 <nyaknodavis318@gmail.com>
2022-10-15 07:28:10 +0100

    hoste

--- Connection/dbConnection.php
+++ Connection/dbConnection.php
@@ -1,10 +1,16 @@
 <?php 
 
    function conString1(){
-      $servername = "localhost";
-      $username = "root";
-      $password = "";
-      $dbname = "rampp";
+      $d = "mysql://bb134d716b6105:7aad4776@us-cdbr-east-06.cleardb.net/heroku_93c90bbbe30b061?reconnect=true";
+      $un = "s";
+      $servername = "us-cdbr-east-06.cleardb.net";
+      $username = "bb134d716b6105";
+      $password = "7aad4776";
+      $dbname = "heroku_93c90bbbe30b061";
+      $servername1 = "localhost";
+      $username1 = "root";
+      $password1 = "";
+      $dbname1 = "rampp"; 
          
       // connect the database with the server
       $conn = new mysqli($servername,$username,$password,$dbname);
